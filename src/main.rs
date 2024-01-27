8e6d891a130ef2e3633132fc102647f798320b5b
AmilsonJunior <junior.amilson@gmail.com>
2024-01-27 03:34:40 -0300

    parse headers and send qr response

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,90 @@
-// Uncomment this block to pass the first stage
 use std::net::UdpSocket;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Debug)]
+struct DNSHeader {
+    id: u16,
+    qr: u8,
+    op_code: u8,
+    aa: u8,
+    tc: u8,
+    rd: u8,
+    ra: u8,
+    z: u8,
+    rcode: u8,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
 
-    // Uncomment this block to pass the first stage
+impl DNSHeader {
+    pub fn new(buffer: &mut [u8]) -> DNSHeader {
+        let id = ((buffer[0] as u16) << 8) | (buffer[1] as u16);
+
+        DNSHeader {
+            id,
+            qr: 1 << 7,
+            op_code: 0,
+            aa: 0,
+            tc: 0,
+            rd: 0,
+            ra: 0,
+            z: 0,
+            rcode: 0,
+            qdcount: 0,
+            ancount: 0,
+            nscount: 0,
+            arcount: 0,
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        bytes.extend(self.id.to_be_bytes());
+        bytes.push(self.qr);
+        bytes.extend(self.op_code.to_be_bytes());
+        bytes.extend(self.aa.to_be_bytes());
+        bytes.extend(self.tc.to_be_bytes());
+        bytes.extend(self.rd.to_be_bytes());
+        bytes.extend(self.ra.to_be_bytes());
+        bytes.extend(self.z.to_be_bytes());
+        bytes.extend(self.rcode.to_be_bytes());
+        bytes.extend(self.qdcount.to_be_bytes());
+        bytes.extend(self.ancount.to_be_bytes());
+        bytes.extend(self.nscount.to_be_bytes());
+        bytes.extend(self.arcount.to_be_bytes());
+
+        bytes
+    }
+}
+
+#[derive(Debug)]
+struct DNSMessage {
+    pub header: DNSHeader,
+}
+
+impl DNSMessage {
+    pub fn new(buffer: &mut [u8]) -> DNSMessage {
+        let header = DNSHeader::new(&mut buffer[..12]);
+
+        DNSMessage { header }
+    }
+}
+
+fn main() {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
 
     loop {
         match udp_socket.recv_from(&mut buf) {
-            Ok((size, source)) => {
-                println!("Received {} bytes from {}", size, source);
-                let response = [];
+            Ok((_, source)) => {
+                let dns_message = DNSMessage::new(&mut buf);
+
+                let msg = &&dns_message.header.to_bytes();
+
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(msg, source)
                     .expect("Failed to send response");
             }
             Err(e) => {
